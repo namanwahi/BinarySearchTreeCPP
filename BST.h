b6bdf0e80886edb11fb6c0847dc3e54b99248142
nw316 <nw316@ic.ac.uk>
2017-08-07 14:07:26 +0100

    interface for BST set up

--- BST.h
+++ BST.h
@@ -2,6 +2,7 @@
 #define BST_H
 
 #include <memory>
+#include <string>
 
 //binary search tree class
 template <class T>
@@ -21,8 +22,20 @@ private:
 	};
 
 	pTreeNode root;
-	
-	
+
+public:
+
+	//insert item of type T into the tree if it is not already in the tree
+	void insert(const T&);
+
+	//remove item of type T from the tree if it is in the tree
+	void remove(const T&);
+
+	//returns true if the tree contains the item of type T
+	bool contains(const T&);
+
+	//converts tree into string form for testing
+	std::string toString();
 };
 
 #endif
